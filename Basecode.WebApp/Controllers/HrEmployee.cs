0a440128661cac93a99bce263b382ce1aad15b36
Elaiza Faye Pepito <elaizafaye.pepito@cit.edu>
2023-06-29 20:39:08 +0800

    Add HrEmployee controller

---
+++ Basecode.WebApp/Controllers/HrEmployee.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Basecode.WebApp.Controllers
+{
+    public class HrEmployee : Controller
+    {
+        public IActionResult HrList()
+        {
+            return View();
+        }
+    }
+}

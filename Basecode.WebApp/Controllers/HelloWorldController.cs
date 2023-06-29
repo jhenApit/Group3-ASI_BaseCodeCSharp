7cbb5f3e2a6ba61bb2a7207c9671747855a43c0e
arvinarar <66163872+arvinarar@users.noreply.github.com>
2023-06-29 17:29:11 +0800

    added Contact Us page
    css is working but image pathing is not working, will try to fix it ASAP

--- Basecode.WebApp/Controllers/HelloWorldController.cs
+++ Basecode.WebApp/Controllers/HelloWorldController.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Text.Encodings.Web;
 
-namespace Basecode.Main.Controllers
+namespace Basecode.WebApp.Controllers
 {
     public class HelloWorldController : Controller
     {

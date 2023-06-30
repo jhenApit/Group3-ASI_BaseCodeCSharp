993f53cb113515a957ca3e1936a89eab7ec489ea
John Mark P. Senening <johnmark.senening@cit.edu>
2023-06-30 11:38:31 +0800

    Update admin dashboard view

---
+++ Basecode.WebApp/Controllers/Admin.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Basecode.WebApp.Controllers
+{
+    public class Admin : Controller
+    {
+        public IActionResult AdminDashboard()
+        {
+            return View();
+        }
+    }
+}

59a6de71e63ab9eb6a0c87cb82922aceb72d1863
Elaiza Faye Pepito <elaizafaye.pepito@cit.edu>
2023-07-02 22:57:36 +0800

    Add HrEmployee model

---
+++ Basecode.Data/Models/HrEmployee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Basecode.Data.Models
+{
+	public class HrEmployee
+	{
+		public int? Id { get; set; }
+		public string? Name { get; set; }
+		public string? Email { get; set; }
+		public string? Password { get; set; }
+	}
+}

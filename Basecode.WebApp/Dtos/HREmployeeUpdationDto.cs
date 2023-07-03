62dfef9b6e0a07e6017c103547220c6f38ab180a
Elaiza Faye Pepito <elaizafaye.pepito@cit.edu>
2023-07-03 17:55:35 +0800

    Add UpdationHREmployee dto

---
+++ Basecode.WebApp/Dtos/HREmployeeUpdationDto.cs
@@ -0,0 +1,14 @@
+﻿using Basecode.WebApp.Models;
+using Humanizer;
+using System.Collections.Specialized;
+
+namespace Basecode.WebApp.Dtos
+{
+    public class HREmployeeUpdationDto
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+    }
+}

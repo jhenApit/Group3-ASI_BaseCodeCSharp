b1297df06e7f7cda2150bad82328eda023c6c4f0
Stella Mariz Palen <stellamariz.palen@cit.edu>
2023-07-04 14:54:33 +0800

    Add ViewJobDescription Page

--- Basecode.WebApp/Controllers/HomeController.cs
+++ Basecode.WebApp/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace Basecode.WebApp.Controllers
             return View();
         }
 
+        public IActionResult ViewJobDescription ()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {

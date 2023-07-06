6330debbca4bea8916b1cbfd91b44be7227bb20b
John Mark P. Senening <johnmark.senening@cit.edu>
2023-07-06 15:39:04 +0800

    Transfer the script to sidebar.js file

---
+++ Basecode.WebApp/wwwroot/js/sidebar.js
@@ -0,0 +1,32 @@
+﻿// remove the 'clicked' class to the clicked element
+let navItems = document.querySelectorAll(".nav-item");
+for (let i = 0; i < navItems.length; i++) {
+    navItems[i].addEventListener("click", (e) => {
+        let clickedElement = e.currentTarget;
+
+        // remove 'clicked' class from all nav-items except the clicked one
+        for (let j = 0; j < navItems.length; j++) {
+            if (navItems[j] !== clickedElement) {
+                navItems[j].classList.remove("clicked");
+            }
+        }
+        clickedElement.classList.toggle("clicked");
+    });
+}
+
+// show the content of the markdown upon clicking na arrow icon
+let arrow = document.querySelectorAll(".arrow");
+for (var i = 0; i < arrow.length; i++) {
+    arrow[i].addEventListener("click", (e) => {
+        let arrowParent = e.target.parentElement.parentElement;
+        arrowParent.classList.toggle("showMenu");
+    });
+}
+
+// close and open the side bar
+let sidebar = document.querySelector(".sidebar");
+let sidebarBtn = document.querySelector(".bx-menu");
+console.log(sidebarBtn);
+sidebarBtn.addEventListener("click", () => {
+    sidebar.classList.toggle("close");
+});

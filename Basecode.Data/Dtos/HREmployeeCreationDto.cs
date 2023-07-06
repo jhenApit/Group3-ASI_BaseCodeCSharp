cab1817594763de2524e62dfe05faa111071c344
Stella Mariz Palen <stellamariz.palen@cit.edu>
2023-07-06 15:46:08 +0800

    Update CreateHrAccount

--- Basecode.Data/Dtos/HREmployeeCreationDto.cs
+++ Basecode.Data/Dtos/HREmployeeCreationDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,10 @@ namespace Basecode.Data.Dtos
 {
     public class HREmployeeCreationDto
     {
-        public string? Name { get; set; }
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(150, ErrorMessage = "Name length can't be more than 150.")]
+        public string Name { get; set; } = string.Empty;
         public string? Email { get; set; }
         public string? Password { get; set; }
         public string? CreatedBy { get; set; }

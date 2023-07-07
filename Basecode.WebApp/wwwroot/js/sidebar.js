e8cd961da16b205a721bfcb9546edb86244911c0
John Mark P. Senening <johnmark.senening@cit.edu>
2023-07-07 15:03:32 +0800

    Modify the sidebar script

--- Basecode.WebApp/wwwroot/js/sidebar.js
+++ Basecode.WebApp/wwwroot/js/sidebar.js
@@ -1,12 +1,21 @@
 ﻿const navItems = document.querySelectorAll(".nav-item");
+let openedSubMenu = null;
 
 function toggleSubMenu(e) {
     const clickedElement = e.currentTarget;
     const subMenu = clickedElement.querySelector(".sub-menu");
     const arrowIcon = clickedElement.querySelector(".arrow");
 
-    if (subMenu) {
+    if (openedSubMenu && openedSubMenu !== subMenu) {
+        openedSubMenu.classList.remove("active");
+        if (arrowIcon) {
+            arrowIcon.classList.remove("rotate");
+        }
+    }
+
+    if (subMenu && !clickedElement.querySelector(".sub-menu").contains(e.target)) {
         subMenu.classList.toggle("active");
+        openedSubMenu = subMenu.classList.contains("active") ? subMenu : null;
     }
 
     for (const item of navItems) {
@@ -24,6 +33,12 @@ function toggleSubMenu(e) {
 
 for (const item of navItems) {
     item.addEventListener("click", toggleSubMenu);
+    const subMenuItems = item.querySelectorAll(".sub-menu-item");
+    for (const subMenuItem of subMenuItems) {
+        subMenuItem.addEventListener("click", (e) => {
+            e.stopPropagation();
+        });
+    }
 }
 
 // close and open the side bar

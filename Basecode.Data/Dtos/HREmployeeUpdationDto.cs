13553a1986eb94097bf451f4afb8a6820c7c13ad
Kathlen Claire Procorato <kathlenclaire.procorato@cit.edu>
2023-07-07 10:39:50 +0800

    Add validation for Edit Hr Account with documentations

--- Basecode.Data/Dtos/HREmployeeUpdationDto.cs
+++ Basecode.Data/Dtos/HREmployeeUpdationDto.cs
@@ -5,15 +5,22 @@ namespace Basecode.Data.Dtos
 {
     public class HREmployeeUpdationDto
     {
+        public int Id { get; set; }
         [JsonProperty(PropertyName = "name")]
-        public string? Name { get; set; }
+        [StringLength(150, MinimumLength = 2, ErrorMessage = "Name must be at least 2 characters.")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Name must contain only letters")]
+        public string? Name { get; set; } = string.Empty;
 
         [Display(Name = "Email")]
+        [MaxLength(50, ErrorMessage = "Email cannot be longer than 50 characters")]
+        [RegularExpression(@"^[a-zA-Z0-9_.+-]+@asi-dev2\.com$", ErrorMessage = "Invalid email address")]
         [JsonProperty(PropertyName = "email")]
-        public string? Email { get; set; }
+        public string? Email { get; set; } = string.Empty;
 
         [Display(Name = "Password")]
         [JsonProperty(PropertyName = "password")]
-        public string? Password { get; set; }
+        [MinLength(6, ErrorMessage = "Password must be at least 6 characters long")]
+        [MaxLength(30, ErrorMessage = "Password cannot be longer than 30 characters")]
+        public string? Password { get; set; } = string.Empty;
     }
 }

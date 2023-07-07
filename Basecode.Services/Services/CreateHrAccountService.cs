d436f289160cfe47fa43c109c76ebb52a939f3c7
John Mark P. Senening <johnmark.senening@cit.edu>
2023-07-07 09:41:44 +0800

    Add other validations for create HR account

--- Basecode.Services/Services/CreateHrAccountService.cs
+++ Basecode.Services/Services/CreateHrAccountService.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Basecode.Services.Services.ErrorHandling;
 
 namespace Basecode.Services.Services
 {
@@ -14,11 +15,46 @@ namespace Basecode.Services.Services
         {
             LogContent logContent = new LogContent();
 
-            if(!hrEmployee.Email.Contains("@asi-dev2.com"))
+            if (hrEmployee.Name.Length < 2)
+            {
+                logContent.Result = false;
+                logContent.ErrorCode = "ERR! Length Validation";
+                logContent.Message = "Name must be at least 2 characters long";
+            }
+            else if (hrEmployee.Name.Length < 2)
+            {
+                logContent.Result = false;
+                logContent.ErrorCode = "ERR! Length Validation";
+                logContent.Message = "Name cannot be longer than 150 characters";
+            }
+            else if (!hrEmployee.Email.Contains("@asi-dev2.com"))
             {
                 logContent.Result = false;
                 logContent.ErrorCode = "INVALID";
                 logContent.Message = "Email is not Alliance Email";
+            }
+            else if (hrEmployee.Email.Length > 50){
+                logContent.Result = false;
+                logContent.ErrorCode = "ERR! Length Validation ";
+                logContent.Message = "Email cannot be longer than 50 characters long";
+            }
+            else if(hrEmployee.Password!.Length < 6)
+            {
+                logContent.Result = false;
+                logContent.ErrorCode = "ERR! Length Validation";
+                logContent.Message = "Password must be at least 6 characters long";
+            }
+            else if (hrEmployee.Password!.Length > 30)
+            {
+                logContent.Result = false;
+                logContent.ErrorCode = "ERR! Length Validation";
+                logContent.Message = "Password cannot be longer than 30 characters";
+            }
+            else
+            {
+                logContent.Result = true;
+                logContent.ErrorCode = "";
+                logContent.Message = "Followed the validations of the input fields.";
             }
 
             return logContent;

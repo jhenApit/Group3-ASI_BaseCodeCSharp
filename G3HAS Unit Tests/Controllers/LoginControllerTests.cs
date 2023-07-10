b34bcb0828235b8551dce67b0d99a5be0dd787af
Jhen Sarah Apit <jhensarah.apit@cit.edu>
2023-07-10 16:39:02 +0800

    Add Initial Login Controller Unit Tests

---
+++ G3HAS Unit Tests/Controllers/LoginControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using Basecode.Services.Interfaces;
+using Basecode.WebApp.Controllers;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G3HAS_Unit_Tests.Controllers
+{
+    public class LoginControllerTests
+    {
+        private readonly Mock<IHrEmployeeService> _serviceMock;
+        private readonly LoginController _controller;
+
+        public LoginControllerTests()
+        {
+            _serviceMock = new Mock<IHrEmployeeService>();
+            _controller = new LoginController(_serviceMock.Object);
+        }
+
+
+    }
+}

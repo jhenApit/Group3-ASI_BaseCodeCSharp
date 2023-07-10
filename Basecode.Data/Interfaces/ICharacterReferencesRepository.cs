04b23199d2e544f101f221338d9d13d8fca9cd5d
takennnusernames <apaleclint@gmail.com>
2023-07-10 22:23:26 +0800

    Merge branch 'dev' into feature/validation-update

---
+++ Basecode.Data/Interfaces/ICharacterReferencesRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Basecode.Data.Models;
+
+namespace Basecode.Data.Interfaces
+{
+    public interface ICharacterReferencesRepository
+    {
+        IQueryable<CharacterReferences> RetrieveAll();
+        CharacterReferences GetByName(string name);
+        void Add(CharacterReferences characterReferences);
+        CharacterReferences GetById(int id);
+    }
+}
---
+++ Basecode.Data/Repositories/AddressRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Basecode.Data.Interfaces;
+using Basecode.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Basecode.Data.Repositories
+{
+    public class AddressRepository : BaseRepository, IAddressRepository
+    {
+        private readonly BasecodeContext _context;
+        public AddressRepository(IUnitOfWork unitOfWork, BasecodeContext context) : base(unitOfWork)
+        {
+            _context = context;
+        }
+        public void Add(Address address)
+        {
+            _context.Addresses.Add(address);
+            _context.SaveChanges();
+        }
+
+        public Address GetByCity(string city)
+        {
+            return _context.Addresses.FirstOrDefault(e => e.City == city);
+        }
+
+        public Address GetById(int id)
+        {
+            return _context.Addresses.FirstOrDefault(e => e.Id == id);
+        }
+
+        public IQueryable<Address> RetrieveAll()
+        {
+            return this.GetDbSet<Address>();
+        }
+    }
+}

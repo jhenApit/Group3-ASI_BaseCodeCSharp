34a1260876fdf2d78d99bf3fa9453f7f5fc103f0
Elaiza Faye Pepito <elaizafaye.pepito@cit.edu>
2023-07-10 16:48:43 +0800

    Add HRController

---
+++ Basecode.WebApp/Controllers/HRController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Basecode.WebApp.Controllers
+{
+    public class HRController : Controller
+    {
+        public IActionResult JobPostList()
+        {
+            return View();
+        }
+
+        public IActionResult CreateJobPost()
+        {
+            return View();
+        }
+    }
+}

1208b13999a556faefd435a9c1bf541c2c5dd341
Kathlen Claire Procorato <kathlenclaire.procorato@cit.edu>
2023-07-10 09:56:29 +0800

    Add Dto for Applicant Model

--- Basecode.Data/Models/Applicant.cs
+++ Basecode.Data/Models/Applicant.cs
@@ -10,43 +10,39 @@ namespace Basecode.Data.Models
 
         public int? JobId { get; set; } // Foreign Key
 
-        [Range(1, 255, ErrorMessage = "ResumeCV must be between 1 and 255.")]
-        public byte ResumeCV { get; set; }
+        [Required(ErrorMessage = "ApplicationDate is required.")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd HH:mm:ss}", ApplyFormatInEditMode = true)]
+        public DateTime ApplicationDate { get; set; }
+
+        public string? Name { get; set; } 
 
         [Required(ErrorMessage = "BirthDate is required.")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime BirthDate { get; set; }
 
-        [StringLength(100, ErrorMessage = "Nationality cannot exceed 100 characters.")]
-        public string? Nationality { get; set; }
+        public byte[]? Photo { get; set; } 
 
         [Phone(ErrorMessage = "MobileNo must be a valid phone number.")]
-        public string? MobileNo { get; set; }
+        public int PhoneNumber { get; set; } 
 
         [EmailAddress(ErrorMessage = "Email must be a valid email address.")]
-        public string? Email { get; set; }
+        public string? Email { get; set; } 
 
-        [Required(ErrorMessage = "ApplicationDate is required.")]
-        [DataType(DataType.DateTime)]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd HH:mm:ss}", ApplyFormatInEditMode = true)]
-        public DateTime ApplicationDate { get; set; }
+        public enum AdditionalInfo 
+        {
+            // Define enum values here
+        }
 
-        public int? Reference1Id { get; set; } // Foreign Key
+        public enum ApplicationStatus 
+        {
+            // Define enum values here
+        }
 
-        public int? Reference2Id { get; set; } // Foreign Key
-
-        public int? AddressId { get; set; } // Foreign Key
-
-        [StringLength(50, ErrorMessage = "StatusId cannot exceed 50 characters.")]
-        public string? StatusId { get; set; } // Foreign Key
-
-        [StringLength(100, ErrorMessage = "UpdatedBy cannot exceed 100 characters.")]
-        public string? UpdatedBy { get; set; }
-
-        [Required(ErrorMessage = "UpdatedTime is required.")]
-        [DataType(DataType.DateTime)]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd HH:mm:ss}", ApplyFormatInEditMode = true)]
-        public DateTime UpdatedTime { get; set; }
+        public enum Requirements 
+        {
+            // Define enum values here
+        }
     }
 }

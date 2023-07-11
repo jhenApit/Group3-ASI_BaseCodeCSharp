292d7f91ceecfc7dddab490059bba7de873315f2
Stella Mariz Palen <stellamariz.palen@cit.edu>
2023-07-11 13:49:34 +0800

    Remove Unusable Methods

--- Basecode.Services/Interfaces/IHrEmployeeService.cs
+++ Basecode.Services/Interfaces/IHrEmployeeService.cs
@@ -19,7 +19,5 @@ namespace Basecode.Services.Interfaces
         void SemiDelete(int id);
         void PermaDelete(int id);
         public LogContent CreateHrAccount(HREmployeeCreationDto hrEmployee);
-        public bool IsNameValid(string name);
-        public bool IsEmailValid(string email);
     }
 }

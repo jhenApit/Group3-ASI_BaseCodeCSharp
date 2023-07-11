54b68cb99d604fec478da7ed4fa271eba96a218a
Stella Mariz Palen <stellamariz.palen@cit.edu>
2023-07-11 14:12:39 +0800

    Fix Errors

--- Basecode.Services/Interfaces/IHrEmployeeService.cs
+++ Basecode.Services/Interfaces/IHrEmployeeService.cs
@@ -19,8 +19,6 @@ namespace Basecode.Services.Interfaces
         void SemiDelete(int id);
         void PermaDelete(int id);
         public LogContent CreateHrAccount(HREmployeeCreationDto hrEmployee);
-        public bool IsNameValid(string name);
-        public bool IsEmailValid(string email);
         public LogContent EditHrAccount(HREmployeeUpdationDto hrEmployee);
     }
 }

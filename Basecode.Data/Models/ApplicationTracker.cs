2924db281e5f724d6cb55f33366b8904e07e8ee1
Kathlen Claire Procorato <kathlenclaire.procorato@cit.edu>
2023-07-11 10:28:41 +0800

    Add ApplicationTracker Repository

---
+++ Basecode.Data/Models/ApplicationTracker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basecode.Data.Models
+{
+    public class ApplicationTracker
+    {
+        public ApplicationTracker() {}
+        public int Id { get; set; }
+        public int ApplicantId { get; set; }
+        public string? TrackerId { get; set; }
+        public enum ApplicationStatus
+        {
+            //values
+        }
+        public ApplicationStatus Status { get; set; }
+    }
+}
---
+++ Basecode.Data/Repositories/ApplicationTrackerRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Basecode.Data.Interfaces;
+using Basecode.Data.Models;
+using static Basecode.Data.Models.ApplicationTracker;
+
+namespace Basecode.Data.Repositories
+{
+    public class ApplicationTrackerRepository : BaseRepository, IApplicationTrackerRepository
+    {
+        private readonly BasecodeContext _context;
+        public ApplicationTrackerRepository(IUnitOfWork unitOfWork, BasecodeContext context) : base(unitOfWork)
+        {
+            _context = context;
+        }
+        public void Add(ApplicationTracker applicationTracker)
+        {
+            _context.ApplicationTrackers.Add(applicationTracker);
+            _context.SaveChanges();
+        }
+
+        public ApplicationTracker GetByStatus(string status)
+        {
+            // Parse the status string to the ApplicationStatus enum
+            if (!Enum.TryParse<ApplicationStatus>(status, out var applicationStatus))
+            {
+                // Handle invalid status value if needed
+                throw new ArgumentException("Invalid status value.");
+            }
+
+            // Retrieve the first ApplicationTracker entity where Status matches the parsed enum value
+            return _context.ApplicationTrackers.FirstOrDefault(e => e.Status == applicationStatus);
+        }
+
+        public ApplicationTracker GetByTrackerId(string trackerId)
+        {
+            var applicationTracker = _context.ApplicationTrackers.FirstOrDefault(e => e.TrackerId == trackerId);
+            return applicationTracker ?? new ApplicationTracker();
+        }
+
+        public IQueryable<ApplicationTracker> RetrieveAll()
+        {
+            return this.GetDbSet<ApplicationTracker>();
+        }
+    }
+}

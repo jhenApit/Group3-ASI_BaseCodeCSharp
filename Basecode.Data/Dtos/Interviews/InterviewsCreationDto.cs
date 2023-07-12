1a2b6c14bc0486cab0378fd47613f4cb1395ae4c
Kathlen Claire Procorato <kathlenclaire.procorato@cit.edu>
2023-07-12 17:46:00 +0800

    Add Interviews model backend implementation

---
+++ Basecode.Data/Dtos/Interviews/InterviewsCreationDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Basecode.Data.Models.Interviews;
+
+namespace Basecode.Data.Dtos.Interviews
+{
+    public class InterviewsCreationDto
+    {
+        public int Id { get; set; }
+        public int ApplicantId { get; set; }
+        public int InterviewerId { get; set; }
+        public IType InterviewType { get; set; }
+        public DateTime InterviewDate { get; set; }
+        public bool Results { get; set; }
+    }
+}

44d3b2e3417b4207594f4f007e20df2b8e4762d0
Kathlen Claire Procorato <kathlenclaire.procorato@cit.edu>
2023-07-12 13:47:01 +0800

    Add Interviewers model, repo, services

---
+++ Basecode.Data/Interfaces/IInterviewersRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Basecode.Data.Models;
+
+namespace Basecode.Data.Interfaces
+{
+    public interface IInterviewersRepository
+    {
+        IQueryable<Interviewers> RetrieveAll();
+        Interviewers? GetByName(string name);
+        void Add(Interviewers Interviewers);
+        Interviewers? GetById(int id);
+        void Update(Interviewers Interviewers);
+        void Delete(int id);
+    }
+}
---
+++ Basecode.Services/Interfaces/IInterviewersService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Basecode.Data.Dtos;
+using Basecode.Data.Dtos.JobPostings;
+using Basecode.Data.Models;
+
+namespace Basecode.Services.Interfaces
+{
+    public interface IInterviewersService
+    {
+        List<Interviewers> RetrieveAll();
+        Interviewers? GetByName(string name);
+        void Add(Interviewers Interviewers);
+        Interviewers? GetById(int id);
+        void Update(InterviewersUpdationDto Interviewers);
+        void Delete(int id);
+    }
+}

8dbefcc551819050c57b258b910eecd952a98a09
John Mark P. Senening <johnmark.senening@cit.edu>
2023-07-13 17:28:25 +0800

    Add scripts for dashboard

---
+++ Basecode.WebApp/wwwroot/js/applicant-chart.js
@@ -0,0 +1,34 @@
+﻿const chartData = {
+    labels: ["Developers", "Designers", "Managers", "Others"],
+    data: [55, 26, 13, 6]
+}
+
+const applicantChart = document.querySelector(".applicant-chart");
+
+const chartOptions = {
+    responsive: true, // Allow the chart to be responsive
+    maintainAspectRatio: false, // Do not maintain the aspect ratio
+    borderWidth: 5,
+    borderRadius: 1,
+    plugins: {
+        legend: {
+            position: 'bottom', // Place the legend at the bottom
+            align: 'center', // Align the legend items to the center
+            fullWidth: true,
+        }
+    }
+};
+
+new Chart(applicantChart, {
+    type: "doughnut",
+    data: {
+        labels: chartData.labels,
+        datasets: [
+            {
+                label: " Job Preference",
+                data: chartData.data,
+            }
+        ],
+    },
+    options: chartOptions,
+});

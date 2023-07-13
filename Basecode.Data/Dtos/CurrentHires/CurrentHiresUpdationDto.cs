78e2a1d269325cf8664bfd5e82c4aab5ba8c0c65
Kathlen Claire Procorato <kathlenclaire.procorato@cit.edu>
2023-07-13 14:40:01 +0800

    Add repo and service for Exam Model

---
+++ Basecode.Data/Dtos/CurrentHires/CurrentHiresUpdationDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Basecode.Data.Models.CurrentHires;
+
+namespace Basecode.Data.Dtos.CurrentHires
+{
+    public class CurrentHiresUpdationDto
+    {
+        public int Id { get; set; }
+        public HStatus HireStatus { get; set; }
+        public DateTime HireDate { get; set; }
+    }
+}
---
+++ Basecode.Data/Interfaces/ICurrentHiresRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Basecode.Data.Models;
+
+namespace Basecode.Data.Interfaces
+{
+    public interface ICurrentHiresRepository
+    {
+        IQueryable<CurrentHires> RetrieveAll();
+        CurrentHires? GetByPositionId(int positionId);
+        void Add(CurrentHires currentHires);
+        CurrentHires? GetById(int id);
+        void Update(CurrentHires currentHires);
+        void Delete(int id);
+        CurrentHires? GetByHireStatus(string status);
+    }
+}

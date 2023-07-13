e788c308ff3a5d1deb4a81a2d5ea2ff3294a5e94
Kathlen Claire Procorato <kathlenclaire.procorato@cit.edu>
2023-07-13 14:12:37 +0800

    Add CurrentHires model

---
+++ Basecode.Data/Models/CurrentHires.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basecode.Data.Models
+{
+    public class CurrentHires
+    {
+        public int Id { get; set; } 
+        public int ApplicantId { get; set; }
+        public int PositionId { get; set; }
+        public enum HStatus
+        {
+            //values
+        }
+        public HStatus HireStatus { get; set; }
+        public DateTime HireDate { get; set; }
+    }
+}

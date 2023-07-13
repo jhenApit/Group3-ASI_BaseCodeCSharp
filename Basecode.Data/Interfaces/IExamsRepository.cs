f9a19795eab1c3f6a99f323b2cd06fe84d36e033
Kathlen Claire Procorato <kathlenclaire.procorato@cit.edu>
2023-07-13 14:02:53 +0800

    Add Exam Model backend implementation

---
+++ Basecode.Data/Interfaces/IExamsRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Basecode.Data.Models;
+
+namespace Basecode.Data.Interfaces
+{
+    public interface IExamsRepository
+    {
+        IQueryable<Exams> RetrieveAll();
+        Exams? GetByApplicantId(int applicantId);
+        void Add(Exams exams);
+        Exams? GetById(int id);
+        void Update(Exams exams);
+        void Delete(int id);
+    }
+}

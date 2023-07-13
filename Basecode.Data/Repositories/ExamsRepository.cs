e3af5fa3224dff2bb7d45cd0f08042fcf401015d
Kathlen Claire Procorato <kathlenclaire.procorato@cit.edu>
2023-07-13 14:02:53 +0800

    Add Exam Model backend implementation

---
+++ Basecode.Data/Repositories/ExamsRepository.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Basecode.Data.Interfaces;
+using Basecode.Data.Models;
+
+namespace Basecode.Data.Repositories
+{
+    public class ExamsRepository : BaseRepository, IExamsRepository
+    {
+        private readonly BasecodeContext _context;
+        public ExamsRepository(IUnitOfWork unitOfWork, BasecodeContext context) : base(unitOfWork)
+        {
+            _context = context;
+        }
+
+        public IQueryable<Exams> RetrieveAll()
+        {
+            return this.GetDbSet<Exams>();
+        }
+
+        public void Add(Exams Exams)
+        {
+            _context.Exams.Add(Exams);
+            _context.SaveChanges();
+        }
+
+        public Exams? GetById(int id)
+        {
+            return _context.Exams.Find(id);
+        }
+
+        public void Update(Exams Exams)
+        {
+            var existingExams = _context.Exams.Find(Exams.Id);
+            if (existingExams != null)
+            {
+                // Update the properties of the existing entity
+                existingExams.ApplicantId = Exams.ApplicantId;
+                existingExams.ProctorId = Exams.ProctorId;
+                existingExams.ExamType = Exams.ExamType;
+                existingExams.Results = Exams.Results;
+                existingExams.ExamDate = Exams.ExamDate;
+
+                // Save the changes
+                _context.SaveChanges();
+            }
+
+        }
+        public void Delete(int id)
+        {
+            var data = _context.Exams.Find(id);
+            if (data != null)
+            {
+                _context.Exams.Remove(data);
+                _context.SaveChanges();
+            }
+        }
+
+        public Exams? GetByApplicantId(int applicantId)
+        {
+            return _context.Exams.FirstOrDefault(e => e.ApplicantId == applicantId);
+        }
+
+    }
+}
---
+++ Basecode.Services/Interfaces/IExamsService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Basecode.Data.Dtos.Exams;
+using Basecode.Data.Models;
+
+namespace Basecode.Services.Interfaces
+{
+    public interface IExamsService
+    {
+        List<Exams> RetrieveAll();
+        Exams? GetByApplicantId(int applicantId);
+        void Add(ExamCreationDto Exams);
+        Exams? GetById(int id);
+        void Update(ExamUpdationDto Exams);
+        void Delete(int id);
+    }
+}

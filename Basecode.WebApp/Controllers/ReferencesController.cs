af760b9ccc4f80dbd905a8353da844c38b31cf02
Stella Mariz Palen <stellamariz.palen@cit.edu>
2023-07-19 14:09:06 +0800

    Add Comments

--- Basecode.WebApp/Controllers/ReferencesController.cs
+++ Basecode.WebApp/Controllers/ReferencesController.cs
@@ -4,11 +4,19 @@ namespace Basecode.WebApp.Controllers
 {
     public class ReferencesController : Controller
     {
+        /// <summary>
+        /// Shows the character reference questions in the first page.
+        /// </summary>
+        /// <returns>Redirect to page 1</returns>
         public IActionResult Page1()
         {
             return View();
         }
 
+        /// <summary>
+        /// Shows the character reference questions in the second page.
+        /// </summary>
+        /// <returns>Redirect to page 2</returns>
         public IActionResult Page2()
         {
             return View();

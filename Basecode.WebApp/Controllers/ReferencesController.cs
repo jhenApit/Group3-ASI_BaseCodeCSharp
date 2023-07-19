10c11bd9c886ca97322291e59d585e17359b5a4c
Stella Mariz Palen <stellamariz.palen@cit.edu>
2023-07-19 11:58:23 +0800

    Add Character References Page

---
+++ Basecode.WebApp/Controllers/ReferencesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Basecode.WebApp.Controllers
+{
+    public class ReferencesController : Controller
+    {
+        public IActionResult Page1()
+        {
+            return View();
+        }
+
+        public IActionResult Page2()
+        {
+            return View();
+        }
+    }
+}

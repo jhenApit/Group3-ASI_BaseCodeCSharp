aacd6d36e1d59fba124c9feb8ba66368da58255c
takennnusernames <apaleclint@gmail.com>
2023-07-20 13:33:24 +0800

    updated migrations with roles and admin account

--- Basecode.Data/Dtos/HREmployeeCreationDto.cs
+++ Basecode.Data/Dtos/HREmployeeCreationDto.cs
@@ -9,6 +9,8 @@ namespace Basecode.Data.Dtos
 {
     public class HREmployeeCreationDto
     {
+        public string UserId { get; set; }
+
         [Required(ErrorMessage = "The 'Name' field is required")]
         [StringLength(150, MinimumLength = 2, ErrorMessage = "Name must be at least 2 characters.")]
         [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Name must contain only letters")]

732b8de4cd229ee1ae0f30c65d38c3f09b43c9ff
Kathlen Claire Procorato <kathlenclaire.procorato@cit.edu>
2023-07-21 10:24:18 +0800

    Add modifications for job posting service

--- Basecode.Services/Services/JobPostingsService.cs
+++ Basecode.Services/Services/JobPostingsService.cs
@@ -138,20 +138,23 @@ namespace Basecode.Services.Services
                         _logContent.Result = false;
                         _logContent.ErrorCode = "400. Edit Failed!";
                         _logContent.Message = "Position Name already exists.";
-                        return _logContent;
+                        //return _logContent;
                     }
+                    _logContent.Result = true;
+                    //return _logContent;
                 }
                 //if job exists and job name entered is the same as the current name
                 _logContent.Result = true;
-                return _logContent;
+                //return _logContent;
             }
             else
             {
                 _logContent.Result = false;
                 _logContent.ErrorCode = "400. Edit Failed!";
                 _logContent.Message = "Job with ID doesn't exist.";
-                return _logContent;
+                //return _logContent;
             }
+            return _logContent;
         }
 
     }

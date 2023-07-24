eee0daf11b60397736ed8802f5be3778becfd37d
Stella Mariz Palen <stellamariz.palen@cit.edu>
2023-07-24 10:25:03 +0800

    Create Add Reference on Both Pages

--- Basecode.WebApp/Controllers/ReferencesController.cs
+++ Basecode.WebApp/Controllers/ReferencesController.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Basecode.Data.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using Basecode.Services.Interfaces;
 
 namespace Basecode.WebApp.Controllers
 {
     public class ReferencesController : Controller
     {
+        public readonly IReferenceFormsService _service;
+
+        public ReferencesController(IReferenceFormsService service)
+        {
+            _service = service;
+        }
         /// <summary>
         /// Shows the character reference questions in the first page.
         /// </summary>
@@ -13,13 +21,32 @@ namespace Basecode.WebApp.Controllers
             return View();
         }
 
-        /// <summary>
-        /// Shows the character reference questions in the second page.
-        /// </summary>
-        /// <returns>Redirect to page 2</returns>
+        [HttpPost]
+        public IActionResult ToPage2(ReferenceFormsCreationDto referenceFormsCreationDto)
+        {
+            TempData["Answer1"] = referenceFormsCreationDto.Answer1;
+            TempData["Answer2"] = referenceFormsCreationDto.Answer2;
+            TempData["Answer3"] = referenceFormsCreationDto.Answer3;
+            TempData["Answer4"] = referenceFormsCreationDto.Answer4;
+            TempData["Answer5"] = referenceFormsCreationDto.Answer5;
+            return RedirectToAction("Page2");
+        }
+
         public IActionResult Page2()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Submit(ReferenceFormsCreationDto referenceFormsCreationDto)
+        {
+            referenceFormsCreationDto.CharacterReferenceId = 1;
+
+            _service.Add(referenceFormsCreationDto);
+
+            TempData.Clear();
+
+            return RedirectToAction("Page1", referenceFormsCreationDto);
         }
     }
 }

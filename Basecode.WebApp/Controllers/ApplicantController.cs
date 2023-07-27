9217c8ddaed38ee7ac6802898cc68df7a44b046a
John Mark P. Senening <johnmark.senening@cit.edu>
2023-07-27 13:03:16 +0800

    Apply related emails to applicant controller

--- Basecode.WebApp/Controllers/ApplicantController.cs
+++ Basecode.WebApp/Controllers/ApplicantController.cs
@@ -1,7 +1,9 @@
 ﻿using Basecode.Data.Models;
 using Basecode.Services.Interfaces;
 using Basecode.Services.Services;
+using Basecode.Services.Utils;
 using Microsoft.AspNetCore.Mvc;
+using static Basecode.Data.Enums.Enums;
 
 namespace Basecode.WebApp.Controllers
 {
@@ -9,11 +11,13 @@ namespace Basecode.WebApp.Controllers
     {
         private readonly IApplicantService _service;
         private readonly IEmailService _emailService;
+        private readonly EmailSender _emailSender;
 
-        public ApplicantController(IApplicantService service, IEmailService emailService)
+        public ApplicantController(IApplicantService service, IEmailService emailService, EmailSender emailSender)
         {
             _service = service;
             _emailService = emailService;
+            _emailSender = emailSender;
         }
 
         /// <summary>
@@ -21,9 +25,13 @@ namespace Basecode.WebApp.Controllers
         /// </summary>
         /// <param name="id">The ID of the applicant.</param>
         /// <returns>The view displaying the track status of the applicant.</returns>
-        public IActionResult TrackStatus(int id)
+        public async Task<IActionResult> TrackStatus(int id)
         {
             Applicant data = _service.GetById(id);
+
+            await _emailSender.SendEmailAsync(EmailType.ScreeningEmailNotificationApplicant, "jm.senening08@gmail.com");
+            await _emailSender.SendEmailAsync(EmailType.ScreeningEmailNotificationHR, "jm.senening08@gmail.com");
+
             return View(data);
         }
 
@@ -50,7 +58,7 @@ namespace Basecode.WebApp.Controllers
         /// </summary>
         /// <returns>The application status view.</returns>
         public IActionResult ApplicationStatus()
-        {
+        {   
             return View();
         }
 
@@ -58,13 +66,15 @@ namespace Basecode.WebApp.Controllers
         /// Displays the application form page.
         /// </summary>
         /// <returns>The application form view.</returns>
-        public IActionResult ApplicationForm()
+        public async Task<IActionResult> ApplicationForm()
         {
-			var recipient = "jm.senening08@gmail.com";
+            /*var recipient = "jm.senening08@gmail.com";
 			var subject = "Application Update";
 			var body = "Your application ID is APPL-1234";
 
-			_emailService.SendEmail(recipient, subject, body);
+			_emailService.SendEmail(recipient, subject, body);*/
+
+            await _emailSender.SendEmailAsync(EmailType.ScreeningEmailApplicantID, "jm.senening08@gmail.com");
 
 			return View();
         }

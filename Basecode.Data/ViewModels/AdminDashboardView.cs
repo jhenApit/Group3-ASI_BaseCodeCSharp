4dd4471feb715331581d976582b0db879f0819ed
takennnusernames <apaleclint@gmail.com>
2023-07-27 16:33:55 +0800

    removed login folder
    unused pages

---
+++ Basecode.Data/ViewModels/AdminDashboardView.cs
@@ -0,0 +1,19 @@
+﻿using Basecode.Data.Dtos;
+using Basecode.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basecode.Data.ViewModels
+{
+    public class AdminDashboardView
+    {
+        public HrEmployee User { get; set; }
+        public JobPostings Jobs { get; set; }
+        public Applicants Candidates { get; set; }
+        public CurrentHires Employees { get; set; }
+        public Interviews Schedules { get; set; }
+    }
+}

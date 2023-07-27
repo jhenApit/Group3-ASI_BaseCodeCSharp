617e5568bd50b83d63ea9fad2583c6671f67ba9a
Jhen Sarah Apit <jhensarah.apit@cit.edu>
2023-07-27 10:46:58 +0800

    Update Models and DTOs

--- Basecode.Data/Models/HrEmployee.cs
+++ Basecode.Data/Models/HrEmployee.cs
@@ -10,10 +10,10 @@ namespace Basecode.Data.Models
 		public int Id { get; set; }
 		public string? UserId { get; set; }
         [ForeignKey("UserId")]
-        public IdentityUser User { get; set; }
-        public string Name { get; set; }
-		public string Email { get; set; }
-		public string Password { get; set; }
+        public IdentityUser? User { get; set; }
+        public string? Name { get; set; }
+		public string? Email { get; set; }
+		public string? Password { get; set; }
         public string? CreatedBy { get; set; }
         public DateTime? CreatedDate { get; set; }
         public string? ModifiedBy { get; set; }

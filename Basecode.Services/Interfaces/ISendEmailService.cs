d5a0e4de38258e43e071868f1b4a9e0b5671bf5f
John Mark P. Senening <johnmark.senening@cit.edu>
2023-07-28 11:37:16 +0800

    New implementation of email service

---
+++ Basecode.Services/Interfaces/ISendEmailService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basecode.Services.Interfaces
+{
+    public interface ISendEmailService
+    {
+        void SendRegretEmail(string recipientEmail, string subject, string body);
+        void SendApprovalEmail(string recipientEmail, string subject, string body);
+    }
+}

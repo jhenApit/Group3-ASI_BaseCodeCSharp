17c58aab4ef2ef2d824fbc6837e971955f4c4582
John Mark P. Senening <johnmark.senening@cit.edu>
2023-07-31 15:14:08 +0800

    Add folder for Applicant Dtos

---
+++ Basecode.Data/Dtos/Applicants/ApplicantsUpdationDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Basecode.Data.Enums.Enums;
+
+namespace Basecode.Data.Dtos.Applicants
+{
+    public class ApplicantsUpdationDto
+    {
+        public ApplicationStatus ApplicationStatus { get; set; }
+    }
+}

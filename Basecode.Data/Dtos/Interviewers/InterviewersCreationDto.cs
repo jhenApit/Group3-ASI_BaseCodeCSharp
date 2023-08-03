c465c8b297a5208c0cd0945b8dc0874d6accf20e
Stella Mariz Palen <stellamariz.palen@cit.edu>
2023-08-03 21:00:31 +0800

    Add Validation for AddInterviewer

--- Basecode.Data/Dtos/Interviewers/InterviewersCreationDto.cs
+++ Basecode.Data/Dtos/Interviewers/InterviewersCreationDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,8 @@ namespace Basecode.Data.Dtos.Interviewers
     public class InterviewersCreationDto
     {
         public int Id { get; set; }
+
+        [RegularExpression(@"^[\sa-zA-z\s]+$", ErrorMessage = "Name must contain only letters")]
         public string? Name { get; set; }
         public string? Email { get; set; }
     }
